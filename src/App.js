15ed99ebdf9704dc5e7f2f42f202d2e9380a7e21
Danitocode <felipedanielc248@gmail.com>
2021-03-14 18:55:37 +0100

    Done state and props

--- src/App.js
+++ src/App.js
@@ -1,71 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Card from './components/Card.js';
-import faker  from 'faker';
+import axios  from 'axios';
 
 function App() {
-  const [cards, setCards] = useState([
-    {
-      id: 'asdfsafsdf',
-      name: 'Damaris Schuster',
-      title: 'International Operations Producer',
-      avatar: 'https://avatars.dicebear.com/api/male/.svg'
-    },
-    {
-      id: 'gfhdfhg',
-      name: 'Bartholome Dietrich',
-      title: 'Future Security Developer',
-      avatar: 'https://avatars.dicebear.com/api/male/.svg'
-    },
-    {
-      id: 'tbdfghfghf',
-      name: 'Pattie Miller',
-      title: 'Forward Metrics AnalystChange Name',
-      avatar: 'https://avatars.dicebear.com/api/male/.svg'
-    }
-  ]);
-  const [showCard, setShowCard] = useState(true);
-  const toggleShowCard = () => setShowCard(!showCard);
-  const deleteCardHandler = (cardIndex) => {
-    const cards_copy = [...cards];
-    console.log(cards_copy);
-    cards_copy.splice(cardIndex, 1);
-    console.log('cards_copy', cards_copy);
-    console.log('cards', cards);
-    setCards(cards_copy);
-  }
+
+
+  const [card, setCard] = useState([]);
+  const [id, setId] = useState(1);
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+    .then(res => {
+      console.log(res.data)
+      setCard(res.data)
+    })
+  }, [id])
+
+
   const changeNameHandler = (event, id) => {
-    //1. which card
-    const cardIndex = cards.findIndex(card => card.id == id);
-    //2. make a copy of the cards
-    const cards_copy = [...cards];
-    //3. change the name of the specific card
-    cards_copy[cardIndex].name = event.target.value;
-    //4. set the cards with the latest version of card copy
-    setCards(cards_copy);
+    //1. make a copy of the cards
+    const card_copy = {...card};
+    //2. change the name of the specific card
+    card_copy.name = event.target.value;
+    //3. set the cards with the latest version of card copy
+    setCard(card_copy);
   }
-  const buttonStyle = {
-    backgroundColor: null
-  }
-
-  if (cards.length < 3) buttonStyle.backgroundColor = 'lightpink';
-  if (cards.length < 2) buttonStyle.backgroundColor = 'red';
-
-  const cardsMarkup = showCard && (
-    cards.map((card, index) => <Card
-      avatar={card.avatar}
-      name={card.name}
-      title={card.title}
-      key={card.id}
-      onDelete={() => deleteCardHandler(index)}
-      onChangeName={(event) => changeNameHandler(event, card.id)}
-    />)
-  )
 
   return (
     <div className="App">
-      <button className="button" style={buttonStyle} onClick={toggleShowCard}>Toggle show/hide</button>
-      {cardsMarkup}
+        <input type="text" value={id} onChange={e => setId(e.target.value)}></input>  
+    <Card
+      avatar={card.avatar}
+      name={card.name}
+      phone={card.phone}
+      title={card.title}
+      key={card.id}
+      onChange={(event) => changeNameHandler(event, card.id)}
+    />
     </div>
   );
 }
